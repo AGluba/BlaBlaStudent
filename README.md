c45fddffcaaf3bf1d2e54ad6bcd76f70d84e1434
242425 <242425@edu.p.lodz.pl>
2024-03-28 13:32:24 +0100

    react + django

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # sumatyw
 Moduł sumatywny
+
+
+Aby uruchomić program należy w folderze backend terminalu wpisać:
+```python manage.py runserver```
+
+A następnie w folderze frontend terminalu wpisać:
+```npm start```
--- backend/sumatyw/urls.py
+++ backend/sumatyw/urls.py
@@ -14,19 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
 
-from sumatyw import views
+from django.contrib import admin
+from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    path('', views.home, name='home'),
-
-    # Include the users app's URLs
-    path('users/', include('users.urls')),
-
-    # Include the trips app's URLs
-    path('trips/', include('trips.urls')),
 ]

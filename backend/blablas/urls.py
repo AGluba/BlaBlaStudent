f282e85e73bcbfb776563834daa2c69887cc1bd5
242425 <242425@edu.p.lodz.pl>
2024-04-29 14:31:41 +0200

    no cos

--- backend/blablas/urls.py
+++ backend/blablas/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from core import views as core_views
 from frontend import urls as frontend_urls
+from offers.views import CustomTokenObtainPairView
 
 
 admin.site.site_header = "Blablas"
@@ -11,7 +12,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('offers.urls')),
     path('activate/<str:uid>/<str:token>/', core_views.your_activation_view, name='activate-user'),
-    path('api/token/', core_views.CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/user/update/', core_views.update_user_profile, name='update-profile'),
     path('account/', include('core.urls')),
     path('auth/jwt/create/', CustomTokenObtainPairView.as_view(), name='custom_jwt_create'),

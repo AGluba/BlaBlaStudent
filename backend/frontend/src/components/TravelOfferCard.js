3a018cb0331253b72499ddd6ba7bb669151e85a5
jakubgaru <242388@edu.p.lodz.pl>
2024-05-04 20:31:21 +0200

    poprawka mapa
    mapa

--- backend/frontend/src/components/TravelOfferCard.js
+++ backend/frontend/src/components/TravelOfferCard.js
@@ -5,7 +5,14 @@ import Typography from '@mui/material/Typography';
 import GoogleMapDisplay from './GoogleMapDisplay';
 
 const TravelOfferCard = ({ offer }) => {
-  const { title, description, place_departure, place_arrival } = offer;
+  const { title, description, place_departure, place_arrival, price, date_departure } = offer;
+  const departureDate = new Date(date_departure).toLocaleString('pl-PL', {
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
+    hour: '2-digit',
+    minute: '2-digit'
+  });
 
   return (
     <Card raised>
@@ -13,10 +20,16 @@ const TravelOfferCard = ({ offer }) => {
         <Typography variant="h5" component="div">{title}</Typography>
         <Typography color="textSecondary" gutterBottom>{description}</Typography>
         <Typography variant="body2" color="textSecondary">
-          Wyjazd: {place_departure}
+          Miejsce wyjazdu: {place_departure}
         </Typography>
         <Typography variant="body2" color="textSecondary">
-          Przyjazd: {place_arrival}
+          Miejsce docelowe: {place_arrival}
+        </Typography>
+        <Typography variant="body2" color="textSecondary">
+          Data wyjazdu: {departureDate}
+        </Typography>
+        <Typography variant="body2" color="textSecondary">
+          Cena: {price.toFixed(2)} zł
         </Typography>
         <GoogleMapDisplay origin={place_departure} destination={place_arrival} />
       </CardContent>
